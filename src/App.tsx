34ab97c47a2dcbee57bfc4159a6dc939a42ece61
cuencaleanneth <cuencaleanneth@gmail.com>
2025-10-07 17:32:17 +0000

    proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-
+import React from 'react';
+import './App.css';
 import VehicleSelector from './components/VehicleSelector';
 import TireSearch from './components/TireSearch';
-import './App.css';
+import motorllantasLogo from './assets/img/motorllantas.png';
+import perfilIcon from './assets/img/perfil.png';
+import compraIcon from './assets/img/compra.png';
 
 function App() {
   return (
     <div className="App">
-      <VehicleSelector />
-      <TireSearch />
+      <header className="App-header">
+        <img src={motorllantasLogo} className="App-logo" alt="logo" />
+        <nav>
+          <a href="#" className="active">INICIO</a>
+          <a href="#">LLANTAS</a>
+          <a href="#">OFERTAS</a>
+          <a href="#">INSTALACIÓN</a>
+          <a href="#">CONTACTO</a>
+        </nav>
+        <div className="header-icons">
+          <img src={perfilIcon} alt="perfil" />
+          <img src={compraIcon} alt="compra" />
+          <span>CO</span>
+        </div>
+      </header>
+      <main>
+        <div className="search-container">
+          <VehicleSelector />
+          <TireSearch />
+        </div>
+        <h1 className="main-title">¡Busca rápido tus llantas!</h1>
+      </main>
     </div>
   );
 }
