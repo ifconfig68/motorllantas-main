e45560a24c021df8555b808a5f69b1d3db709392
cuencaleanneth <cuencaleanneth@gmail.com>
2025-10-07 17:50:30 +0000

    cambio

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './InfoSection.css';
-import fotocarro from '../assets/img/fotocarro.jpg';
+import photocar from '../assets/img/photocar.JPG';
 
 function InfoSection() {
   return (
     <div className="info-section">
-      <img src={fotocarro} alt="Offroad truck" />
+      <div className="info-content">
+        <div className="info-image">
+          <img src={photocar} alt="Offroad truck" />
+        </div>
+        <div className="info-text">
+          <h2>LLANTAS PARA CADA AVENTURA</h2>
+          <p>
+            Encuentra la llanta perfecta para tu vehículo, sin importar el terreno.
+            Explora nuestra amplia selección de llantas de alta calidad para autos,
+            camionetas, OTR, motos y cuatrimotos.
+          </p>
+          <a href="#" className="button">VER CATÁLOGO</a>
+        </div>
+      </div>
     </div>
   );
 }
