43366c6ed5f8976930ff7871effa492c5eea4215
cuencaleanneth <cuencaleanneth@gmail.com>
2025-10-07 22:08:54 +0000

    cambio

--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -203,8 +203,8 @@ const ProductCarousel: React.FC = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 5,
+          slidesToScroll: 5,
         },
       },
       {
