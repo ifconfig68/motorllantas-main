50c8bfb0b89901a9cd1a54bfc972e462eb2eb95b
cuencaleanneth <cuencaleanneth@gmail.com>
2025-10-23 21:17:59 +0000

    cambio

--- src/pages/Installation.tsx
+++ src/pages/Installation.tsx
@@ -6,8 +6,6 @@ import installation2 from '../assets/img/instalacion2.JPG';
 const Installation: React.FC = () => {
   return (
     <div className="page-container installation-page">
-      <div className="page-title-container">
-      </div>
       <div className="installation-content">
         <h2 className="installation-title">Instalación Segura y precisa</h2>
         
@@ -17,7 +15,7 @@ const Installation: React.FC = () => {
 
         <div className="installation-section">
           <div className="installation-image">
-            <img src={installation1} alt="Instalación de llantas 1" />
+            <img src={installation1} alt="Instalación de llantas 1" className="installation-img-1"/>
           </div>
           <div className="installation-text">
             <p>Durante la instalación, aplicamos tecnología de última generación para lograr un montaje y balanceo preciso. Cada llanta se ajusta con herramientas profesionales, calibradas al detalle, evitando daños en los rines y garantizando un desempeño óptimo. Nuestro equipo verifica presión, válvulas y torque de seguridad, asegurando que cada pieza quede en su punto exacto. Además, te mantenemos informado de cada paso, para que sepas exactamente qué se está haciendo con tu vehículo y por qué es importante hacerlo con especialistas certificados como los de <strong>Motorllantas</strong>.</p>
@@ -29,7 +27,7 @@ const Installation: React.FC = () => {
             <p>Al finalizar, realizamos una revisión final y prueba de calidad para que salgas con la tranquilidad de que tus llantas están instaladas correctamente y listas para rendir al máximo. Te entregamos recomendaciones de mantenimiento y calibración para que prolongues su vida útil y disfrutes de una conducción segura. En  <strong>Motorllantas</strong> no solo cambiamos llantas, te ayudamos a cuidar tu inversión y tu seguridad en la vía. Agenda tu instalación hoy mismo y siente la diferencia de un servicio profesional, rápido y garantizado.</p>
           </div>
           <div className="installation-image">
-            <img src={installation2} alt="Instalación de llantas 2" />
+            <img src={installation2} alt="Instalación de llantas 2" className="installation-img-2"/>
           </div>
         </div>
 
